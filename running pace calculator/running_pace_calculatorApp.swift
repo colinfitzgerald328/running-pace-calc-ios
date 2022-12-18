1494cf1df4aab8865644c38d8be3601a1ca86df3
Colin FitzGerald <colinfitzgerald328@gmail.com>
2022-12-17 22:32:05 -0800

    Initial Commit

---
+++ running pace calculator/running_pace_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  running_pace_calculatorApp.swift
+//  running pace calculator
+//
+//  Created by Colin FitzGerald on 12/17/22.
+//
+
+import SwiftUI
+
+@main
+struct running_pace_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ running pace calculatorUITests/running_pace_calculatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  running_pace_calculatorUITestsLaunchTests.swift
+//  running pace calculatorUITests
+//
+//  Created by Colin FitzGerald on 12/17/22.
+//
+
+import XCTest
+
+final class running_pace_calculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
