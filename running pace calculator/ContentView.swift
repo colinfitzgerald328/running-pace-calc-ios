317702bb7e701cd877ae1b681f1ffe6b49e725ea
Colin FitzGerald <colinfitzgerald328@gmail.com>
2022-12-18 09:15:52 -0800

    adding fully functional build

--- running pace calculator/ContentView.swift
+++ running pace calculator/ContentView.swift
@@ -1,26 +1,43 @@
-//
-//  ContentView.swift
-//  running pace calculator
-//
-//  Created by Colin FitzGerald on 12/17/22.
-//
+import UIKit
 
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+class ViewController: UIViewController {
+    
+    // Declare three UITextFields for the inputs
+    let input1TextField = UITextField()
+    let input2TextField = UITextField()
+    let input3TextField = UITextField()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set up the input text fields
+        input1TextField.placeholder = "Input 1"
+        input1TextField.borderStyle = .roundedRect
+        input2TextField.placeholder = "Input 2"
+        input2TextField.borderStyle = .roundedRect
+        input3TextField.placeholder = "Input 3"
+        input3TextField.borderStyle = .roundedRect
+        
+        // Add the input text fields to the view
+        view.addSubview(input1TextField)
+        view.addSubview(input2TextField)
+        view.addSubview(input3TextField)
+        
+        // Set up the layout constraints for the input text fields
+        input1TextField.translatesAutoresizingMaskIntoConstraints = false
+        input1TextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        input1TextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        input1TextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        
+        input2TextField.translatesAutoresizingMaskIntoConstraints = false
+        input2TextField.topAnchor.constraint(equalTo: input1TextField.bottomAnchor, constant: 20).isActive = true
+        input2TextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        input2TextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        
+        input3TextField.translatesAutoresizingMaskIntoConstraints = false
+        input3TextField.topAnchor.constraint(equalTo: input2TextField.bottomAnchor, constant: 20).isActive = true
+        input3TextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        input3TextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
--- running pace calculator/running_pace_calculatorApp.swift
+++ running pace calculator/running_pace_calculatorApp.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import UIKit
+import Foundation
 
 @main
 struct running_pace_calculatorApp: App {
@@ -13,5 +15,93 @@ struct running_pace_calculatorApp: App {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+struct ContentView: View {
+    @State var input1: String = ""
+    @State var input2: String = ""
+    @State var input3: String = ""
+    @State var result: String = ""
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Running Time:")
+                TextField("Enter a value", text: $input1)
+                    .padding(8)
+                    .background(Color.gray)
+            }
+            HStack {
+                Text("Running Pace:")
+                TextField("Enter a value", text: $input2)
+                    .padding(8)
+                    .background(Color.gray)
+            }
+            HStack {
+                Text("Running Distance:")
+                TextField("Enter a value", text: $input3)
+                    .padding(8)
+                    .background(Color.gray)
+            }
+            Button(action: {
+                struct ToDoRequestModel: Codable {
+                    var running_time: String
+                    var running_pace: String
+                    var running_distance: String
+                }
+                
+                struct ToDoResponseModel: Codable {
+                    var converted_result: String
+                    var operation: String
+                }
+                
+                let url = URL(string: "https://evening-spire-30745.herokuapp.com/convert")
+                guard let requestUrl = url else { fatalError() }
+                
+                var request = URLRequest(url: requestUrl)
+                request.httpMethod = "POST"
+                
+                // Set HTTP Request Header
+                request.setValue("application/json", forHTTPHeaderField: "Accept")
+                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                
+                let newTodoItem = ToDoRequestModel(running_time: self.input1, running_pace: self.input2, running_distance: self.input3)
+                do{
+                    let jsonData = try JSONEncoder().encode(newTodoItem)
+                    request.httpBody = jsonData
+                } catch let jsonErr{
+                    print(jsonErr)
+                }
+                
+                
+                
+                let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                    
+                    if let error = error {
+                        print("Error took place \(error)")
+                        return
+                    }
+                    guard let data = data else {return}
+                    
+                    do{
+                        let todoItemModel = try JSONDecoder().decode(ToDoResponseModel.self, from: data)
+                        print("Response data:\n \(todoItemModel)")
+                        print("todoItemModel Title: \(todoItemModel.converted_result)")
+                        self.result = todoItemModel.converted_result
+                    }catch let jsonErr{
+                        print(jsonErr)
+                    }
+                    
+                    
+                }
+                task.resume()
+                
+            }) {
+                Text("Calculate")
+            }
+            Text(result)
+        }
+        .padding(16)
     }
 }
